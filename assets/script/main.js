0c669c950bd2a86e6cee961eea748960b4559fb4
isaiah <isaiahshekinah@gmail.com>
2026-05-20 12:53:22 +0100

    updated code

---
+++ assets/script/main.js
@@ -0,0 +1,11 @@
+let name = 'Dean';
+//let this is what we call variable type
+//name, variable name
+//variable data
+
+//const, let, var
+//var, you can redeclare and you can reassign 
+var age = 10;
+var age = 20; //reassign and we redeclaring // console.log(age);
+
+let color = 'blue'; //
